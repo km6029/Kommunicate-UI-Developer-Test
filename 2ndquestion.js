5aca1f7e5ffc7e0a55a962fb7d37d07c39da9a6a
km6029 <91561312+km6029@users.noreply.github.com>
2022-07-29 13:15:47 +0530

    Create 2ndquestion.js

---
+++ 2ndquestion.js
@@ -0,0 +1,10 @@
+function getUrlParameterValue(url, parameter) {
+  if(url.search(parameter)){
+    var words = url.split(/&|=/);
+    var numberIndex = words.findIndex((word) => word == parameter);
+    var nextWord = words[numberIndex + 1];
+    console.log(nextWord)
+  }
+}
+var url =“https://www.kommunicate.io/poweredby?utm_source=https://www.kommunicate.io/&utm_medium=webplugin&utm_campaign=poweredby”;
+getUrlParameterValue(url,"utm_campaign")
